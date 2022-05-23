5fb9704a4a1d1ddbb74b134ebbb6334ef62f44ba
Andre <andrebennett106@gmail.com>
2022-05-23 17:07:39 -0400

    create shoes model and create delete shoes model

--- backend/drf_jwt_backend/settings.py
+++ backend/drf_jwt_backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'authentication.apps.AuthenticationConfig',
     'corsheaders',
-    'shoes.apps.ShoesConfig'  
+    'shoes'  
 ]
 
 MIDDLEWARE = [
--- backend/shoes/models.py
+++ backend/shoes/models.py
@@ -4,11 +4,13 @@ from authentication.models import User
 
 # Create your models here.
 
-# <<<<<<<<<<<<<<<<< EXAMPLE FOR STARTER CODE USE <<<<<<<<<<<<<<<<<
+
 
 
 class Shoe(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    make = models.CharField(max_length=30)
+    brand = models.CharField(max_length=30)
     model = models.CharField(max_length=100)
-    price = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    color = models.CharField(max_length=30)
+    size = models.IntegerField()
